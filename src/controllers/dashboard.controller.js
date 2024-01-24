5aa66f21023e82d42dd2f47e56b9ffea255641f8
Jawad Ahmed <144881314+jaawad81@users.noreply.github.com>
2024-01-24 13:03:30 +0500

    Add channel stats and channel videos endpoints

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -8,10 +8,40 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    // get channel id from cookies
+    // count total views of all the videos owned by this user
+    // count total subscriber 
+    // count total views on all  his videos
+    //count total likes on all his videos
+    const userId=req.user._id;
+    const videos=await Video.find({owner:userId});
+    if(!videos){
+        throw new ApiError(404,"No videos found");
+    }
+    let totalViews=0;
+    let totalLikes=0;
+    videos.forEach(video=>{
+        totalViews+=video.views;
+        totalLikes+=video.likes;
+    })
+    const subscriptions=await Subscription.find({channel:userId});
+    const totalSubscribers=subscriptions.length;
+    const response=new ApiResponse(200,{totalViews,totalLikes,totalSubscribers},"Stats found");
+    res.status(200).json(response);
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
+    // get channel id from cookies
+    // get all videos of this channel owned by user either published or unpublished
+    const userId=req.user._id;
+    const videos=await Video.find({owner:userId});
+    if(!videos){
+        throw new ApiError(404,"No videos found");
+    }
+    const response=new ApiResponse(200,videos,"Videos found");
+    res.status(200).json(response);
+
 })
 
 export {
